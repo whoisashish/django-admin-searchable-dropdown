2263d90bd20af93ab9064d04b6c2413f147e724b
Ashish Yadav <yadavobito@gmail.com>
2021-08-16 10:32:39 +0530

    Filter Path

--- django_admin_searchable_dropdown/filters.py
+++ django_admin_searchable_dropdown/filters.py
@@ -8,20 +8,20 @@ from django.contrib.admin.filters import (
 
 
 class SimpleDropdownFilter(SimpleListFilter):
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+    template = 'django_admin_searchable_dropdown/dropdown_filter.html'
 
 
 class DropdownFilter(AllValuesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+    template = 'django_admin_searchable_dropdown/dropdown_filter.html'
 
 
 class ChoiceDropdownFilter(ChoicesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+    template = 'django_admin_searchable_dropdown/dropdown_filter.html'
 
 
 class RelatedDropdownFilter(RelatedFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+    template = 'django_admin_searchable_dropdown/dropdown_filter.html'
 
 
 class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+    template = 'django_admin_searchable_dropdown/dropdown_filter.html'
